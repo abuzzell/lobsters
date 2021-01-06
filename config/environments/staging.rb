4ef1c1db54d8b5a02600bca681cb19066817eecd
Andrew Buzzell <andrewbuzzell@gmail.com>
2021-01-06 08:08:18 -0400

    assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Should default to true, but doesn't...
   config.assets.digest = true
