d00f292b6a548b08c769fa405b5cda299b153ee2
Andrew Buzzell <andrewbuzzell@gmail.com>
2021-06-24 14:54:03 -0300

    update mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   #default :from => "#{Rails.application.name} <nobody@#{Rails.application.domain}>"
-  default :from => "eiten@ethicalintelligence.co"
+  default :from => "info@ethicalintelligence.co"
 end
